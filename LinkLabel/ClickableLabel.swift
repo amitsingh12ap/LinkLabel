e6aaa0bf370ca01ee5fc57075bd4de93bc467d8e
Amit Kr Singh <amit1.singh@africa.airtel.com>
2020-09-17 15:16:35 +0530

    first commit

---
+++ LinkLabel/ClickableLabel.swift
@@ -0,0 +1,21 @@
+//
+//  ClickableLabel.swift
+//  LinkLabel
+//
+//  Created by 13216146 on 17/09/20.
+//  Copyright © 2020 13216146. All rights reserved.
+//
+
+import UIKit
+
+class ClickableLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
